014e5c00ab902a83706bbf89cf23f49b531ed58f
Bulat Khisamov <bulatkhisamov25142@gmail.com>
2022-02-02 21:28:45 +0300

    added signin sign up

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const path = require('path');
+const userRouter = require('./routes/userRouter');
+const imageRouter = require('./routes/imageRouter');
+const mainRouter = require('./routes/mainRouter');
 
 const app = express();
 const PORT = 3000;
 
-
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,6 +30,9 @@ app.use((req, res, next) => {
   res.locals.username = req.session?.user; // optional chaining operator
   next();
 });
+
+app.use('/user', userRouter);
+app.use('/', mainRouter);
 
 app.listen(PORT, () => {
   console.log('Hello');
--- db/models/image.js
+++ db/models/image.js
@@ -4,14 +4,8 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Image extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       this.belongsTo(models.User, { foreignKey: 'user_id' });
-      // define association here
     }
   }
   Image.init({
